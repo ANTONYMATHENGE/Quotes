a737a9d144a299e8405d8b7f8ec9f7e5117cf801
ANTONY MATHENGE <tonnykogi@gmail.com>
2020-09-27 23:03:01 +0300

    create logic for adding a quote

--- src/app/highlight.directive.spec.ts
+++ src/app/highlight.directive.spec.ts
@@ -6,3 +6,11 @@ describe('HighlightDirective', () => {
     expect(directive).toBeTruthy();
   });
 });
+import { HighlightDirective } from './highlight.directive';
+
+describe('HighlightDirective', () => {
+  it('should create an instance', () => {
+    const directive = new HighlightDirective();
+    expect(directive).toBeTruthy();
+  });
+});
