eb84a48d7913029e5f3334372d2f7c22cfa70ecd
ANTONY MATHENGE <tonnykogi@gmail.com>
2020-09-27 19:40:21 +0300

    create quote body component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuoteComponent } from './quote/quote.component';
+import { QuoteBodyComponent } from './quote-body/quote-body.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    QuoteComponent
+    QuoteComponent,
+    QuoteBodyComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/quote-body/quote-body.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { QuoteBodyComponent } from './quote-body.component';
+
+describe('QuoteBodyComponent', () => {
+  let component: QuoteBodyComponent;
+  let fixture: ComponentFixture<QuoteBodyComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ QuoteBodyComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(QuoteBodyComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
